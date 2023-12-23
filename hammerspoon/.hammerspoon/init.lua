6ba611725aa4d91d6cf2076f4824beefdbec434a
huangliang1203 <huangliang1203@gmail.com>
2023-12-23 12:07:36 +0800

    add config for hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,3 +1,9 @@
+-- auto reload
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+hs.alert.show("Config loaded")
+
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)

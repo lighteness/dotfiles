45bd0ff07a2b872d702a0472f08fafe67d7685bb
huangliang1203 <huangliang1203@gmail.com>
2023-12-23 12:07:56 +0800

    switch inputmethod

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,6 +4,27 @@ spoon.ReloadConfiguration:start()
 hs.alert.show("Config loaded")
 
 
+-- switch inputmethod
+local function Chinese()
+  -- hs.keycodes.setMethod("Pinyin - Simplified")
+  hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
+end
+
+local function English()
+  -- hs.keycodes.setLayout("U.S.")
+  hs.keycodes.currentSourceID("com.apple.keylayout.US")
+end
+
+hs.hotkey.bind({"ctrl","shift"}, "j", function()
+  English()
+end)
+
+hs.hotkey.bind({"ctrl","shift"}, "k", function()
+  Chinese()
+end)
+
+
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)

9c7b959b502974f5c6a002a63c53f22ca1a8a159
huangliang1203 <huangliang1203@gmail.com>
2023-12-24 21:05:46 +0800

    shortcut for input method

--- hammerspoon/.hammerspoon/modules/inputmethods.lua
+++ hammerspoon/.hammerspoon/modules/inputmethods.lua
@@ -1,3 +1,5 @@
+require 'modules.shortcut'
+
 local function Chinese()
   -- hs.keycodes.setMethod("Pinyin - Simplified")
   hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
@@ -8,11 +10,8 @@ local function English()
   hs.keycodes.currentSourceID("com.apple.keylayout.US")
 end
 
-hs.hotkey.bind({"ctrl","shift"}, "j", function()
-  English()
-end)
+hs.hotkey.bind(input_methods.abc.prefix, input_methods.abc.key, input_methods.abc.message,English)
+hs.hotkey.bind(input_methods.chinese.prefix, input_methods.chinese.key, input_methods.chinese.message, Chinese)
 
-hs.hotkey.bind({"ctrl","shift"}, "k", function()
-  Chinese()
-end)
+-- hs.hotkey.bind({"option"}, "k", Chinese)
 
---
+++ hammerspoon/.hammerspoon/modules/shortcut.lua
@@ -0,0 +1,5 @@
+
+input_methods = {
+   abc  = { prefix = {"Option"}, key = "J", message="ABC"},
+   chinese  = { prefix = {"Option"}, key = "K", message="拼音"}
+}
